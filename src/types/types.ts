5e7dd367c2d7344c8b9bf491e3ce9e055effa8e4
Sergey Suprun <serg.suprun@gmail.com>
2021-08-14 18:45:12 +0300

    feat: add file with types defenition'

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export type serverRes = {
+    description: string,
+    id: string;
+    name: string,
+    thumbnail: { path: string, extension: string};
+}
+export type heroes = {
+    description: string,
+    id: string;
+    heroName: string,
+    thumbnail: string;
+}
