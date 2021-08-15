a405c5a228f67ca0d5bddb979a445c133eebb0e4
Sergey Suprun <serg.suprun@gmail.com>
2021-08-15 22:03:31 +0300

    feat: add data render and dynamic search

--- src/containers/container.tsx
+++ src/containers/container.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MediaCard from '../components/card';
 import './container.css';
+import MarvelApi from '../api/api';
+import { serverRes } from '../types/types';
 
-const Container = () => {
+interface IProps {
+  name: string;
+}
+
+const Container = (props: IProps) => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (props.name) {
+        const request = await MarvelApi.getHeroByName(props.name);
+        setCards(request.data.data.results);
+        return request;
+      }
+      const request = await MarvelApi.getHeroes(20);
+      setCards(request.data.data.results);
+      return request;
+    }
+    fetchData();
+  }, [props]);
+  const elms = cards.map((item: serverRes) => {
+    // eslint-disable-next-line max-len
+    return <MediaCard id={item.id} name={item.name} thumbnail={item.thumbnail.path} key={item.id} />;
+  });
   return (
     <div className="container">
-      <MediaCard heroName="Iron Man" id="1009368" thumbnail="http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55" description="Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man." />
-      <MediaCard heroName="Iron Man" id="1009368" thumbnail="http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55" description="Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man." />
-      <MediaCard heroName="Iron Man" id="1009368" thumbnail="http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55" description="Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man." />
+      {elms}
     </div>
   );
 };
