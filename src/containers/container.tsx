bc6d1be24bd0680a6b62b78d06a68e119dde2b66
Sergey Suprun <serg.suprun@gmail.com>
2021-08-16 18:20:26 +0300

    feat: add spinner/loader implementation

--- src/containers/container.tsx
+++ src/containers/container.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import MediaCard from '../components/card';
+import Spinner from '../components/spinner';
 import './container.css';
 import MarvelApi from '../api/api';
 import { serverRes } from '../types/types';
@@ -10,8 +11,9 @@ interface IProps {
 
 const Container = (props: IProps) => {
   const [cards, setCards] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
     async function fetchData() {
       if (props.name) {
         const request = await MarvelApi.getHeroByName(props.name);
@@ -22,12 +24,18 @@ const Container = (props: IProps) => {
       setCards(request.data.data.results);
       return request;
     }
-    fetchData();
+    fetchData().then(() => {
+      setTimeout(() => { setLoading(false); }, 500);
+    });
   }, [props]);
-  const elms = cards.map((item: serverRes) => {
+  const elms = cards.map((item: serverRes, index) => {
+    const timeout = (500 + index * 100);
     // eslint-disable-next-line max-len
-    return <MediaCard id={item.id} name={item.name} thumbnail={item.thumbnail.path} key={item.id} />;
+    return <MediaCard id={item.id} name={item.name} thumbnail={item.thumbnail.path} key={item.id} timeout={timeout} />;
   });
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <div className="container">
       {elms}
