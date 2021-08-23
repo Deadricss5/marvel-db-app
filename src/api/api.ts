7e51a9d5c5a59ee3583a0cadaa26109e84baaa8b
Sergey Suprun <serg.suprun@gmail.com>
2021-08-23 10:10:40 +0300

    fix: refactor constructor and add offset and limit params in request functions

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,30 +1,39 @@
 import axios from 'axios';
 import md5 from 'md5';
+import { IMarvelApi } from '../types/types';
 
 function generateApiKey(): string {
-  const keyPublic: string = '78e53f63a7b15473e3c6ce8e5e3c7f71';
-  const keyPrivate: string = 'ded020dc438316c8741477116c2c1b19d8035aca';
+  // const keyPublic = '78e53f63a7b15473e3c6ce8e5e3c7f71';
+  // const keyPrivate = 'ded020dc438316c8741477116c2c1b19d8035aca';
+  // 32926644ce@emailnax.com
+  // const keyPublic = '97d07dafe404020c6a6e67d6a9ee45f2';
+  // const keyPrivate = 'bd4444d6fedc5ef782c32e663a1fa94accc98975';
+  // 56378302b3@emailnax.com
+  // const keyPublic = '2fc18fcd157de715be9a7f511b839ae4';
+  // const keyPrivate = '60ad714c669921d03f54756411500d750385ce70';
+  // 2d1a91bd2a@emailnax.com
+  const keyPublic = '0eae8530cd8dcf4e3f02d5aea4380a1f';
+  const keyPrivate = '5bd75bc5d706b69eb73833f9f77b8b28e207e3f7';
   const ts: number = Date.now();
 
   const hash: string = md5(ts + keyPrivate + keyPublic);
 
   return `ts=${ts}&apikey=${keyPublic}&hash=${hash}`;
 }
-
-class MarvelApi {
+class MarvelApi implements IMarvelApi {
   baseURL: string;
 
   constructor() {
-    const baseURL: string = 'https://gateway.marvel.com:443/v1/public';
-    this.baseURL = baseURL;
+    this.baseURL = 'https://gateway.marvel.com:443/v1/public';
   }
 
-  getHeroes(limit: number) {
+  getHeroes(offset = 0) {
     return axios.get(
       `${this.baseURL}/characters?&${generateApiKey()}`,
       {
         params: {
-          limit,
+          limit: 20,
+          offset,
         },
       },
     );
@@ -36,20 +45,22 @@ class MarvelApi {
     );
   }
 
-  getHeroByName(name: string) {
+  getHeroByName(name: string, offset = 0) {
     return axios.get(
       `${this.baseURL}/characters?&${generateApiKey()}`,
       {
         params: {
           nameStartsWith: name,
+          limit: 20,
+          offset,
         },
       },
     );
   }
 
-  getComics(heroId: number, limit: number = 20, offset: number = 0) {
+  getComics(heroId: number) {
     return axios.get(
-      `${this.baseURL}/characters/${heroId}/comics?limit=${limit}&offset=${offset}${generateApiKey()}`,
+      `${this.baseURL}/characters/${heroId}/comics?${generateApiKey()}`,
     );
   }
 }
