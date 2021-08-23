d7ba46a9ebb185ebc7cb8b5aaca948e6d1d4fa9f
Sergey Suprun <serg.suprun@gmail.com>
2021-08-23 10:14:40 +0300

    refactor: remove unused code due to changes in the structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './components/header';
-import Container from './containers/container';
-// import Spinner from './components/spinner';
+import MainPage from './containers/main-page';
 
-function App() {
-  const [searchInput, setSearchInput] = useState('');
-
-  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTimeout(() => { setSearchInput(e.target.value); }, 100);
-  };
+export default function App(): JSX.Element {
   return (
     <div className="app-container">
       <Header />
-      <input
-        onChange={onChangeSearch}
-        placeholder="Enter hero name"
-        className="input"
-      />
-      <Container name={searchInput} />
+      <MainPage value />
     </div>
   );
 }
-export default App;
