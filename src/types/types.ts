7e7fc7c9f1911f2bcb44906a54ac53d0acd13fe8
Sergey Suprun <serg.suprun@gmail.com>
2021-08-23 10:30:42 +0300

    feat: add file with types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import { AxiosResponse } from 'axios';
+
 export interface serverRes {
     description: string,
     id: string;
@@ -9,4 +11,12 @@ export interface heroes {
     id: string;
     name: string,
     thumbnail: string;
+}
+export interface IMarvelApi {
+    baseURL: string;
+
+    getHeroes(limit: number, offset: number): Promise<AxiosResponse<void>>;
+    getHeroById(heroId: number): Promise<AxiosResponse<void>>;
+    getHeroByName(name: string, offset: number): Promise<AxiosResponse<void>>;
+    getComics(heroId: number, limit: number, offset: number): Promise<AxiosResponse<void>>;
 }
