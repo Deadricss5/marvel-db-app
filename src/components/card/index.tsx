a4466f952ce49edf110f1f48855d29ffec19b168
Sergey Suprun <serg.suprun@gmail.com>
2021-09-07 20:07:05 +0300

    refactor: change aproach for import from material-ui

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import Typography from '@material-ui/core/Typography';
-import Zoom from '@material-ui/core/Grow';
+import {
+  CardMedia,
+  CardContent,
+  CardActionArea,
+  Typography,
+  Zoom,
+} from '@material-ui/core/';
 import './card.css';
 
 interface IProps {
