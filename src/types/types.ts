40144cdaecf314e94ec16920137ef2e30917533f
Sergey Suprun <serg.suprun@gmail.com>
2021-09-07 20:33:36 +0300

    refactor: remove unused code

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,12 +6,6 @@ export interface serverResponse {
     name: string,
     thumbnail: { path: string, extension: string};
 }
-export interface heroes {
-    description: string,
-    id: string;
-    name: string,
-    thumbnail: string;
-}
 export interface IMarvelApi {
     baseURL: string;
     getHeroes(limit: number, offset: number): Promise<AxiosResponse<void>>;
