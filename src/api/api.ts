b1ea6bb2e476f9c16cf467d28e3d609389cddb02
Sergey Suprun <serg.suprun@gmail.com>
2021-09-08 16:48:36 +0300

    refactor: move keys to .env file

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,23 @@
 import axios from 'axios';
 import md5 from 'md5';
 import { IMarvelApi } from '../types/types';
-import { keyPrivate, keyPublic, baseURL } from './.env/variables';
 
-function generateApiKey(): string {
-  const timeStamp: number = Date.now();
+const { REACT_APP_PUBLIC_KEY, REACT_APP_PRIVATE_KEY, REACT_APP_BASE_URL } = process.env;
 
-  const hash: string = md5(timeStamp + keyPrivate + keyPublic);
+const timeStamp: number = Date.now();
+const hash: string = md5(timeStamp + <string>REACT_APP_PRIVATE_KEY + REACT_APP_PUBLIC_KEY);
+const API_KEY = `ts=${timeStamp}&apikey=${REACT_APP_PUBLIC_KEY}&hash=${hash}`;
 
-  return `ts=${timeStamp}&apikey=${keyPublic}&hash=${hash}`;
-}
 class MarvelApi implements IMarvelApi {
   baseURL: string;
 
   constructor() {
-    this.baseURL = baseURL;
+    this.baseURL = <string>REACT_APP_BASE_URL;
   }
 
   getHeroes(offset = 0) {
     return axios.get(
-      `${this.baseURL}/characters?&${generateApiKey()}`,
+      `${this.baseURL}/characters?&${API_KEY}`,
       {
         params: {
           limit: 20,
@@ -31,13 +29,13 @@ class MarvelApi implements IMarvelApi {
 
   getHeroById(heroId: number) {
     return axios.get(
-      `${this.baseURL}/characters/${heroId}?${generateApiKey()}`,
+      `${this.baseURL}/characters/${heroId}?${API_KEY}`,
     );
   }
 
   getHeroByName(name: string, offset = 0) {
     return axios.get(
-      `${this.baseURL}/characters?&${generateApiKey()}`,
+      `${this.baseURL}/characters?&${API_KEY}`,
       {
         params: {
           nameStartsWith: name,
@@ -50,7 +48,7 @@ class MarvelApi implements IMarvelApi {
 
   getComics(heroId: number) {
     return axios.get(
-      `${this.baseURL}/characters/${heroId}/comics?${generateApiKey()}`,
+      `${this.baseURL}/characters/${heroId}/comics?${API_KEY}`,
     );
   }
 }
