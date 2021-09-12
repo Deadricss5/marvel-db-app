3e4a1358d37278749a6f072d8d7cd72d61b17e06
Sergey Suprun <serg.suprun@gmail.com>
2021-09-12 21:44:24 +0300

    fix: rewrite class component as a function component because it doesn't use state and lifecycle methods'

--- src/containers/main-page/index.tsx
+++ src/containers/main-page/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import Container from '../search_and_cards/container';
 import './main-page.css';
 
-interface IState {
-  searchInput: string;
-}
 interface IProps {
-  value: string;
   location: {
     search: string,
   };
@@ -21,47 +17,25 @@ interface IProps {
   };
 }
 
-class MainPage extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      searchInput: '',
-    };
-  }
-
-  componentDidMount(): void {
-    const { location } = this.props;
-    const params = new URLSearchParams(location.search);
-    if (params.has('name')) {
-      this.setState({ searchInput: params.get('name') || '' });
-    }
-  }
-
-  render(): JSX.Element {
-    const { searchInput } = this.state;
-    const { history, location } = this.props;
-    const params = new URLSearchParams(location.search);
-    const page: number = Number(params.get('page')) || 1;
-    const heroName: string = params.get('name') || '';
-    return (
-      <div className="main-page">
-        <input
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            this.setState({ searchInput: e.target.value });
-            if (e.target.value) {
-              history.push(`/?name=${e.target.value}`);
-            } else {
-              history.push('/');
-            }
-          }}
-          placeholder="Enter hero name"
-          className="input"
-          value={searchInput}
-        />
-        <Container name={searchInput || heroName} page={page} />
-      </div>
-    );
-  }
+export default function MainPage({ location, history }: IProps): JSX.Element {
+  const params = new URLSearchParams(location.search);
+  const page: number = Number(params.get('page')) || 1;
+  const heroName: string = params.get('name') || '';
+  return (
+    <div className="main-page">
+      <input
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          if (e.target.value) {
+            history.push(`/?name=${e.target.value}`);
+          } else {
+            history.push('/');
+          }
+        }}
+        placeholder="Enter hero name"
+        className="input"
+        value={heroName}
+      />
+      <Container name={heroName} page={page} />
+    </div>
+  );
 }
-
-export default MainPage;
